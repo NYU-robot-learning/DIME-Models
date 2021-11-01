20d01179da539aaba3b0a21544b9003b62d2c649
SridharPandian <sripan172@gmail.com>
2021-11-01 17:52:51 -0400

    Refactored code.

--- Imitation_NN/non_parametric_imitation.py
+++ Imitation_NN/non_parametric_imitation.py
@@ -1,7 +1,4 @@
-import os
 import torch
-
-from Imitation_NN.utils.dataloader import load_train_test_data
 
 class ImitationNearestNeighbors():
     def __init__(self,
@@ -11,19 +8,22 @@ class ImitationNearestNeighbors():
         # Setting the device
         self.device = torch.device(device)
 
-        # Loading dataset and transfering it to device         
-        train_states, train_actions, self.test_states, self.test_actions = load_train_test_data(data_path)
+        # Setting the state action pairs to None
+        self.states = None
+        self.actions = None
 
-        self.train_states, self.train_actions = train_states.to(device), train_actions.to(device)
+    def get_data(self, states, actions):
+        self.states = states.to(self.device)
+        self.actions = actions.to(self.device)
 
     def getNearestNeighbors(self, input_state, k):
         # Comparing the dataset shape and state
-        assert input_state.shape != self.train_state[0].shape, "There is a data shape mismatch: \n Shape of loaded dataset: {} \n Shape of current state: {}"
+        assert input_state.shape != self.states[0].shape, "There is a data shape mismatch: \n Shape of loaded dataset: {} \n Shape of input state: {}".format(self.states[0].shape, input_state.shape)
 
         # Getting the k-Nearest Neighbor actions
-        state_diff = self.train_states - torch.tensor(input_state).to(self.device)
+        state_diff = self.states - torch.tensor(input_state).to(self.device)
         l2_diff = torch.norm(state_diff, dim=1)
-        k_nn_actions = self.train_actions[torch.argsort(l2_diff)[:k+1]]
+        k_nn_actions = self.actions[torch.argsort(l2_diff)[:k+1]]
 
         return k_nn_actions
 
@@ -32,6 +32,6 @@ class ImitationNearestNeighbors():
         k_nn_actions = self.getNearestNeighbors(input_state, k)
 
         # Getting the mean value from the set of nearest neighbor states
-        mean_action = torch.mean(k_nn_actions).cpu().detach()
+        mean_action = torch.mean(k_nn_actions, 0).cpu().detach()
 
         return mean_action
